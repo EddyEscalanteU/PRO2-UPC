74fec0879e833ddc47f1bcec167a08382fff248a
Eddy Escalante Ustariz <31227566+EddyEscalanteU@users.noreply.github.com>
2023-11-08 19:37:41 -0400

    proyecto de ejemplo

---
+++ DOCENTE/08112023/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("sumaDosNumeros");
+            int x = 20;
+            int y = 21;
+            int respuesta = sumaDosNumeros(x, y);
+            Console.WriteLine(respuesta);
+        }
+
+        static int sumaDosNumeros(int a, int b){
+            return a + b;
+        }
+    }
+}
