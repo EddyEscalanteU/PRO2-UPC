52f45210981837d995321ecab3abff1847b2e22c
jesg9000 <64978051+jesg9000@users.noreply.github.com>
2023-11-07 21:02:00 -0400

    Video2

---
+++ jorge/Presentacion1/Presentacion1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Presentacion1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //ESTE ES UN PROGRAMA DE HOLA MUNDO
+            /* Este es un comentario 
+             * de bloque */
+            Console.WriteLine("Hola mundo");
+            Console.ReadKey();
+        }
+    }
+}
