3a96eba316fb302ca0e34931d94c788ece5b9465
Eddy Escalante Ustariz <31227566+EddyEscalanteU@users.noreply.github.com>
2023-11-08 19:40:54 -0400

    Update Program.cs

--- DOCENTE/08112023/Program.cs
+++ DOCENTE/08112023/Program.cs
@@ -9,10 +9,19 @@
             int y = 21;
             int respuesta = sumaDosNumeros(x, y);
             Console.WriteLine(respuesta);
+
+            int respuesta2 = multiplicarDosNumeros(x, y);
+            Console.WriteLine(respuesta2);
         }
 
         static int sumaDosNumeros(int a, int b){
             return a + b;
+        }
+
+
+        
+        static int multiplicarDosNumeros(int a, int b){
+            return a * b;
         }
     }
 }
