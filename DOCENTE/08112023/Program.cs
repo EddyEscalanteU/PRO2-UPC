99f2eb9cb7ebbb43c7531dab4c2bfdbef34ea0fc
Eddy Escalante Ustariz <31227566+EddyEscalanteU@users.noreply.github.com>
2023-11-08 19:44:15 -0400

    Update Program.cs

--- DOCENTE/08112023/Program.cs
+++ DOCENTE/08112023/Program.cs
@@ -1,6 +1,5 @@
 ﻿namespace HelloWorld
 {
-    //new class
     class Program
     {
         static void Main(string[] args)
@@ -10,17 +9,10 @@
             int y = 21;
             int respuesta = sumaDosNumeros(x, y);
             Console.WriteLine(respuesta);
-
-            int respuesta2 = multiplicarDosNumeros(x, y);
-            Console.WriteLine(respuesta2);
         }
 
         static int sumaDosNumeros(int a, int b){
             return a + b;
-        }
-
-        static int multiplicarDosNumeros(int a, int b){
-            return a * b;
         }
     }
 }
