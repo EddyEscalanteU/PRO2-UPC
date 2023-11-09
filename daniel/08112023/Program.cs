d6adac61362aea11e8fca1bfa0c04be47530be89
= <df84696@gmail.com>
2023-11-09 14:45:27 -0400

    proyecto de ejemplo

---
+++ daniel/08112023/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("sumaDosNumeros");
+            int x = 10;
+            int y = 20;
+            int respuesta = sumaDosNumeros (x, y);
+            Console.WriteLine(respuesta);
+        }
+        static int sumaDosNumeros(int a, int b){
+            return a + b;
+        }
+    }
+}
