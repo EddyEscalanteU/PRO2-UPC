0bcdcc560815a5ed1c563cf03664ef2ef6c3d085
Nelson-Ayna <149635642+Nelson-Ayna@users.noreply.github.com>
2023-11-09 00:01:07 -0400

    mi primera tarea

---
+++ ayna/Tarea 1/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            int resp = CalcularAreaCuadrado(10);
+            Console.WriteLine(resp);
+        }
+        
+        static int CalcularAreaCuadrado(int L){
+            int resultado = L*L;
+            return resultado;
+        }
+
+    }
+}
