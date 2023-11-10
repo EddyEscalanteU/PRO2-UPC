a5723fc3f8334d7233f724bfffb431623ce545a1
JASSEPH <149634812+JASSEPH@users.noreply.github.com>
2023-11-09 21:41:22 -0400

    avanse 2

---
+++ jcastillo/practica 2/mascotas.cs
@@ -0,0 +1,46 @@
+//Realizar una clase con el nombre
+// "Mascotas", que tenga las siguientes propiedades: (Nombre, Edad, Nombrepropietario, color, Categoria). */
+//Escribir los siguientes metodos:
+//Darnombre (String nombre)
+//asignarcolor (String color)
+
+public class Mascota
+{
+    public String nombre;
+    public int edad;
+    public String Nombrepropietario;
+    public String color;
+    public String Categoria;
+
+    public Mascota()
+    {
+        Nombre = "sin nombre";
+        Edad = 0;
+        Propietario =new persona();
+        Color = "sin color";
+        Categoria = "sin categoria";
+    }    
+
+    public void DarNombre(String valor)
+    {      
+        nombre = valor;
+    }
+
+    public void Asignarcolor(String valor)
+    {
+        color = valor;
+    }
+
+    public String Mostrar()
+    {
+        return " mascota { Nombre: " + Nombre + ", "+
+        "Edad: " + edad + ", " +
+        "Nombre Propietario: " + propietario.Mostrar() + ", " +
+        "Color: " + color + ", " +
+        "Categoria: " + categoria + ", " + " } "
+
+
+    }
+
+}       
+        
