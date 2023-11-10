4705bf03b4dfe70098ef2b0c4d86e7bc97d37547
Eddy Escalante Ustariz <31227566+EddyEscalanteU@users.noreply.github.com>
2023-11-09 21:32:10 -0400

    avance 2

--- ignacio/practica 2/Program.cs
+++ ignacio/practica 2/Program.cs
@@ -2,10 +2,23 @@
     class Program{
         public static void Main(string[] args)
         {
+            /*
             Mascota mascota1 = new Mascota();
-            Console.WriteLine(mascota1.Nombre);
+            Console.WriteLine(mascota1.Mostrar());
             mascota1.DarNombre("firulais");
-            Console.WriteLine(mascota1.Nombre);
+            Console.WriteLine(mascota1.Mostrar());
+
+            Persona varibleJuan = new Persona();
+            Console.WriteLine(varibleJuan.Mostrar());
+            Persona pedro = new Persona(123, "El Pedro", 10);
+            Console.WriteLine(pedro.Mostrar());
+            */
+
+            Mascota myMascota =  new Mascota();
+            Console.WriteLine(myMascota.Mostrar());
+
+
+
         }
     }
 }
