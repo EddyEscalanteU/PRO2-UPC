e4b61db05beaeb257c7f9754d68a2ee2c91977ee
AMAYADIEGO <amayadiego839@gmail.com>
2023-11-10 19:00:32 -0400

    Actividad5

--- damaya/actividad 5/materia.cs
+++ damaya/actividad 5/materia.cs
@@ -1,28 +1,28 @@
 public class Materia
 {
     public string Grupo;
-    public string materia;
+    public string Nombremateria;
     public string Docente;
     public int Periodo;
     public int Hora;
     public Materia()
     {
         Grupo = "Sin nombre";
-        materia = "Sin nombre";
+        Nombremateria = "Sin nombre";
         Docente = "Sin nombre";
         Periodo = 0;
         Hora = 0;
     }
     public Materia(string grup, string asignatura, string teacher, int period, int hour){
         Grupo = grup;
-        materia = asignatura;
+        Nombremateria = asignatura;
         Docente = teacher;
         Periodo = period;
         Hora = hour;
     }
     public string Mostrar(){
         return "Materia { Grupo: " + Grupo + ", " +
-        "materia: " + materia + ", " +
+        "materia: " + Nombremateria + ", " +
         "Docente: " + Docente + ", " +
         "periodo: " + Periodo + ", " +
         "hora, " + Hora + "}";
