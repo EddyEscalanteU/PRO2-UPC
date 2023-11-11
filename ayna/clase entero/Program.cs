eff1c229306b8c00259785c46406431f1ae88414
Nelson-Ayna <149635642+Nelson-Ayna@users.noreply.github.com>
2023-11-10 20:28:50 -0400

    avance

---
+++ ayna/clase entero/Program.cs
@@ -0,0 +1,17 @@
+﻿﻿namespace ClaseEntero
+{
+    public class Program{
+        public static void Main(string[] args)
+        {
+            Entero Nro = new Entero(21);
+            Nro.N = 243111138;
+            Console.WriteLine("N: " + Nro.N);
+            //bool res = Nro.EsPar_Mejorado();
+            //bool res = Nro.EsMultiploN(2);
+            //Console.WriteLine("EsMultiploN: " + res);
+
+            ///Nro.MostrarDigitos();
+            Nro.MostrarDigPares();
+        }
+    }
+}
--- ayna/practica2/Program.cs
+++ ayna/practica2/Program.cs
@@ -1,13 +1,17 @@
-﻿namespace Prantica2
-{
-    class Programa
-    {
-        static void Main(string [] args)
+﻿namespace Practica2{
+    class Program{
+        public static void Main(string[] args)
         {
-            Mascota mascota1 = new mascota();
-            console.WriteLine(mascota1.Nombre);
+            Mascota mascota1 = new Mascota();
+            Console.WriteLine(mascota1.Nombre);
             mascota1.DarNombre("sin nombre");
-            console.WriteLine(mascota1.nombre);
+            Console.WriteLine(mascota1.Nombre);
+        }
+        public string mostra(){
+            return "Nombre: "+ Nombre + ", " +
+            "Edad: "+ Edad + ", " +
+            "Color: "+ Color + ", " +
+            "Edad: "+ edad;
 
         }
     }
