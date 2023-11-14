c2b53c6dfc6cfb343b8d33ee0ea3cfa6a8c47014
Carlosalbertomv <132417628+Carlosalbertomv@users.noreply.github.com>
2023-11-13 21:54:48 -0400

    prac

---
+++ carlos/practica clase 8/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Clase8
+{
+    public class Program{
+        public static void Main(string[] args)
+        {
+            ClaseArray A = new ClaseArray(5);
+            A.ArrayEnteros[0] = 1;
+            A.ArrayEnteros[1] = 9;
+            A.ArrayEnteros[2] = 10;
+            A.ArrayEnteros[3] = 2;
+            A.ArrayEnteros[4] = 7;
+            A.MostrarArray();
+            //Console.WriteLine("-----------------------");
+            //A.OrdenamientoBurbuja();
+            //A.MostrarArray();
+            //Console.WriteLine("-----------------------");
+            //A.Intercalar_Uno();
+            //A.MostrarArray();
+            Console.WriteLine("-----------------------");
+            A.DeleteByPosition(1);
+            A.MostrarArray();
+            A.DeleteByPosition(3);
+            A.MostrarArray();
+            
+        }
+    }
+}
