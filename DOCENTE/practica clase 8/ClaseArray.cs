edb2a7637f30a2e3fcc4bcdfb5b6c65ad809b0bf
Eddy Escalante Ustariz <31227566+EddyEscalanteU@users.noreply.github.com>
2023-11-13 21:50:24 -0400

    v5

--- DOCENTE/practica clase 8/ClaseArray.cs
+++ DOCENTE/practica clase 8/ClaseArray.cs
@@ -1,3 +1,5 @@
+using System.Diagnostics.Contracts;
+
 public class ClaseArray
 {
     //Propiedades
@@ -80,6 +82,71 @@ public class ClaseArray
         }
         ArrayEnteros = B.ArrayEnteros;
     }
+
+    public void UnirArray(ClaseArray B)
+    {
+        int n1 = ArrayEnteros.Length;
+        int n2 = B.ArrayEnteros.Length;
+        int newN = n1 + n2;
+        ClaseArray C = new ClaseArray(newN);
+        for (int i = 0; i <= n1 - 1; i++)
+        {
+            C.ArrayEnteros[i] = ArrayEnteros[i];
+        }
+        for (int j = 0; j <= n2 - 1; j++)
+        {
+            C.ArrayEnteros[n1 + j] = B.ArrayEnteros[j];
+        }
+        ArrayEnteros = C.ArrayEnteros;
+
+    }
+
+
+    public void SumarElementosArray()
+    {
+        int suma = 0;
+        int n = ArrayEnteros.Length;
+        for (int i = 0; i <= n - 1; i++)
+        {
+            suma = suma + ArrayEnteros[i];
+        }
+        Console.WriteLine(suma);
+    }
+    public void MultiplicarElementosArray()
+    {
+        int multi = 1;
+        int n = ArrayEnteros.Length;
+        for (int i = 0; i <= n - 1; i++)
+        {
+            multi = multi * ArrayEnteros[i];
+        }
+        Console.WriteLine(multi);
+    }
+
+    public void ElevarCuadrado()
+    {
+        int n = ArrayEnteros.Length;
+        for (int i = 0; i <= n - 1; i++)
+        {
+            ArrayEnteros[i] = (ArrayEnteros[i] * ArrayEnteros[i]);
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
