4255d3c83d4110bfd2eb609ffef8a3bb9980fb34
Eddy Escalante Ustariz <31227566+EddyEscalanteU@users.noreply.github.com>
2023-11-13 21:04:18 -0400

    v4

--- DOCENTE/practica clase 8/Program.cs
+++ DOCENTE/practica clase 8/Program.cs
@@ -3,13 +3,25 @@
     public class Program{
         public static void Main(string[] args)
         {
-            ClaseArray A = new ClaseArray(4);
-            A.ArrayEnteros[0] = 85;
-            A.ArrayEnteros[1] = 74;
-            A.ArrayEnteros[2] = 544;
-            A.ArrayEnteros[3] = 12;
-            //A.ArrayEnteros[4] = 111;//error
+            ClaseArray A = new ClaseArray(5);
+            A.ArrayEnteros[0] = 1;
+            A.ArrayEnteros[1] = 9;
+            A.ArrayEnteros[2] = 10;
+            A.ArrayEnteros[3] = 2;
+            A.ArrayEnteros[4] = 7;
             A.MostrarArray();
+            //Console.WriteLine("-----------------------");
+            //A.OrdenamientoBurbuja();
+            //A.MostrarArray();
+            //Console.WriteLine("-----------------------");
+            //A.Intercalar_Uno();
+            //A.MostrarArray();
+            Console.WriteLine("-----------------------");
+            A.DeleteByPosition(1);
+            A.MostrarArray();
+            A.DeleteByPosition(3);
+            A.MostrarArray();
+            
         }
     }
 }
