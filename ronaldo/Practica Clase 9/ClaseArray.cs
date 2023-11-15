2da0b8a4f7e62025ddafdce1fecf5a443b5788ab
ronaldozega <blackronaldo201@gmail.com>
2023-11-15 15:39:13 -0400

    v1

---
+++ ronaldo/Practica Clase 9/ClaseArray.cs
@@ -0,0 +1,13 @@
+public class ClaseArray
+{
+    string [] ArrayEnteros;
+
+    public ClaseArray()
+    {
+        ArrayEnteros = new string [0];
+    }
+    public ClaseArray(int cant)
+    {
+        ArrayEnteros = new string [cant];
+    }
+}
---
+++ ronaldo/Practica Clase 9/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace MYnamespace
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            
+        }
+
+    }
+}
