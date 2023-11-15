9c9209deb750d62c565956af9ebce0cc5102a537
gonzalo1106 <149978831+gonzalo1106@users.noreply.github.com>
2023-11-14 21:19:28 -0400

    clase 9 array v2

--- grodriguez/PRACTICA CLASE 9/clase array.cs
+++ grodriguez/PRACTICA CLASE 9/clase array.cs
@@ -1,18 +1,16 @@
-public class ClaseArray{
-   public string[] ArrayCadenas;
+public class ClaseArray
+{
+    public string[] ArrayCadenas;
 
-    public ClaseArray(){
-
+    public ClaseArray()
+    {
         ArrayCadenas = new string[0];
-
     }
-    public ClaseArray(int cant){
 
+    public ClaseArray(int cant)
+    {
         ArrayCadenas = new string[cant];
     }
-
-
-
 
     public void MostrarArray()
     {
@@ -23,4 +21,43 @@ public class ClaseArray{
         }
         Console.WriteLine(resp + "]");
     }
+
+    public void GenerarAbcdario(){
+        for (int i = 97; i <= 122; i++)
+        {
+            int numero = i;
+            char letra = (char)numero;
+            Console.WriteLine(numero + ": " + letra);
+        }
+    }
+
+    public void GenerarAbcdarioMAYUS2(){
+        int cantAbcdario = (90-65) + 1;
+        ArrayCadenas = new string[cantAbcdario];
+
+        for (int i = 65; i <= 90; i++)
+        {
+            int numero = i;
+            char letra = (char)numero;
+            //Console.WriteLine(numero + ": " + letra);
+            ArrayCadenas[i - 65] = letra + ""; 
+        }
+        MostrarArray();
+    }
+    
+    public void GenerarAbcdarioMAYUS(){
+        int cantAbcdario = (90-65) + 1;
+        ClaseArray B = new ClaseArray(cantAbcdario);
+
+        for (int i = 65; i <= 90; i++)
+        {
+            int numero = i;
+            char letra = (char)numero;
+            //Console.WriteLine(numero + ": " + letra);
+            B.ArrayCadenas[i - 65] = letra + ""; 
+        }
+        B.MostrarArray();
+    }
+
+
 }
