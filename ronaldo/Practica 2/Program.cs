cc5a9d501ae9261a4360add837dbab3ead9625c5
ronaldozega <blackronaldo201@gmail.com>
2023-11-15 02:25:21 -0400

    Mi Mascota

---
+++ ronaldo/Practica 2/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Mascotas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Mascota mascota1 = new Mascota();
+            Console.WriteLine(mascota1.nombre);
+            mascota1.Darnombre("Kiara");
+            mascota1.AsginarEdad(01);
+            mascota1.AsignarColor("Blanco");
+            mascota1.AsiganarCategoria("BullTerrier");
+            mascota1.AsiganarNombredelPropietario("Ronaldo Zegarrundo Gutierrez");
+
+            Console.WriteLine("NOMBRE:");
+            Console.WriteLine(mascota1.nombre);
+            Console.WriteLine("EDAD:");
+            Console.WriteLine(mascota1.edad);
+            Console.WriteLine("COLOR:");
+            Console.WriteLine(mascota1.color);
+            Console.WriteLine("CATEGORIA:");
+            Console.WriteLine(mascota1.categoria);
+            Console.WriteLine("NOMBRE DEL PROPIETARIO");
+            Console.WriteLine(mascota1.NombredelPropietario);
+            
+        }
+    }
+}
