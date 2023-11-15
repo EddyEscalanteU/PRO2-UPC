d178d7a73941af0373ec89d7c114a5e973490d09
ronaldozega <blackronaldo201@gmail.com>
2023-11-15 11:25:36 -0400

    v2

--- ronaldo/Clase 8/Program.cs
+++ ronaldo/Clase 8/Program.cs
@@ -5,8 +5,15 @@
     {
         public static void Main(string[] args)
         {
-        
-        
+            claseArray A = new claseArray (4) ;
+            A.ArrayEnteros [0] = 85 ;
+            A.ArrayEnteros [1] = 74 ;
+            A.ArrayEnteros [2] = 544 ;
+            A.ArrayEnteros [3] = 12 ;
+
+            //A.ArrayEnteros [4] = 111 ; // error
+
+            A.MostrarArray();
         }
     }
 }
