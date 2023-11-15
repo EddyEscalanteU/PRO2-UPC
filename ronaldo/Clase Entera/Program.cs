3293b37168dc4ad635508a9f5c128d2ed5a1c7d6
ronaldozega <blackronaldo201@gmail.com>
2023-11-15 05:24:39 -0400

    Entero V4

---
+++ ronaldo/Clase Entera/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace ClaseEntero;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Entero Nro = new Entero (21);
+        Nro.N = 243111138 ;
+        Console.WriteLine("N:" + Nro.N);
+        //bool res = Nro.EsPar_Mejorado();
+        // bool res = Nro.EsMultiploN(7);
+        //  Console.WriteLine("EsMutiploN: " + res );
+
+        //Nro.MostrarDigitos();
+        Nro.MostrarDigPares();
+    }
+}
