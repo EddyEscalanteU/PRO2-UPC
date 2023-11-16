edfd9d4d39d71f9c5b4097f54a6005a82b60dc04
DannyCarita <150048492+DannyCarita@users.noreply.github.com>
2023-11-15 20:10:36 -0400

    nuevo

--- danny/PRIMER_PARCIAL/Program.cs
+++ danny/PRIMER_PARCIAL/Program.cs
@@ -1,1 +1,20 @@
-﻿﻿
+﻿﻿namespace SumaArray
+{
+    class Program
+    {
+        static void Main(strig)[] args)
+        {
+            int tam = 5;
+            int[] numeros = new int[tam];
+            
+
+
+
+
+
+
+
+
+
+
+
