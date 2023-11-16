d40a664f2b83ca5d5c548ce7d61620bd928b99e0
ronaldozega <blackronaldo201@gmail.com>
2023-11-15 21:47:14 -0400

    v1 pregunta 2

---
+++ ronaldo/PRIMER PARCIAL/PREGUNTA 2/parcial.cs
@@ -0,0 +1,27 @@
+﻿namespace Parcial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Estudiante estudiante1 = new Estudiante();
+            Console.WriteLine(estudiante1.nombreCompleto);
+            estudiante1.Asignarnombre("MIguel");
+            estudiante1.AsginarEdad(21);
+            estudiante1.Asiganagenero("Masculino");
+            estudiante1.AsginarID(123);
+            estudiante1.AsginarCarnet(1267);
+
+            Console.WriteLine("NOMBRE:");
+            Console.WriteLine(estudiante1.nombreCompleto);
+            Console.WriteLine("EDAD:");
+            Console.WriteLine(estudiante1.edad);
+            Console.WriteLine("GENERO:");
+            Console.WriteLine(estudiante1.genero);
+            Console.WriteLine("ID:");
+            Console.WriteLine(estudiante1.ID);
+            Console.WriteLine("CARNET");
+            Console.WriteLine(estudiante1.carnet);;
+        }
+    }
+}
