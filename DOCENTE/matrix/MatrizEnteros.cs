7b80f6b5d47ddab33b12acf09bbeeb4de709df6f
Eddy Escalante Ustariz <31227566+EddyEscalanteU@users.noreply.github.com>
2023-11-17 20:25:17 -0400

    v2

--- DOCENTE/matrix/MatrizEnteros.cs
+++ DOCENTE/matrix/MatrizEnteros.cs
@@ -1,11 +1,18 @@
 public class MatrizEnteros
 {
+    //Elemento de la Matriz
     public int[,] M;
+
+    //Cantidad de filas
     public int fila;
+
+    //Cantidad de columnas
     public int columna;
+
+    //Valor por defecto
     public int defaultValue = -1;
 
-
+    //El constructor vacio
     public MatrizEnteros()
     {
         M = new int[0, 0];
@@ -13,6 +20,7 @@ public class MatrizEnteros
         columna = 0;
     }
 
+    //El constructor dandole la cantidad X y Y
     public MatrizEnteros(int cantX, int cantY)
     {
         //Dimensionar la matriz
@@ -21,6 +29,7 @@ public class MatrizEnteros
         columna = cantY;
     }
 
+    //Cargar la matriz con valores por defecto
     public void CargarMatrizDefault()
     {
         //Dar valores por defecto a la matriz
@@ -33,6 +42,7 @@ public class MatrizEnteros
         }
     }
 
+    //Mostrar el contenido de la matriz
     public void MostrarMatriz()
     {
         string res = "";
