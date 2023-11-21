342b5f50c71063203d162fba85f77e1458a801ce
Carlosalbertomv <132417628+Carlosalbertomv@users.noreply.github.com>
2023-11-20 22:02:02 -0400

    ffgf

--- carlos/Matriz/Program.cs
+++ carlos/Matriz/Program.cs
@@ -4,13 +4,14 @@
     {
         public static void Main(string[] args)
         {
-            MatrizEnteros Matriz = new MatrizEnteros(4, 4);
-            //M.defaultValue = 7;
+            MatrizEnteros Matriz = new MatrizEnteros(5, 5);
+            Matriz.defaultValue = 0;
             Matriz.CargarMatrizDefault();
             Matriz.MostrarMatriz();
+            /*
             Console.WriteLine("---------------------------");
             Matriz.M[2, 1] = 5;         //opcion 1
-           // Matriz.Insertar(2, 1, 5);   //opcion 2
+            // Matriz.Insertar(2, 1, 5);   //opcion 2
             Matriz.M[3,2] = 3;
             Matriz.M[3, 0] = 7;
             Matriz.M[1, 3] = 3;
@@ -30,7 +31,9 @@
             Console.WriteLine("---------------------------");
             //Matriz.LlenarMatriz_v1();
             //Matriz.MostrarMatriz();
-            Matriz.LlenarMatriz_v4();
+            */
+            Console.WriteLine("---------------------------");
+            Matriz.MatrizSerie_Mas3();
             Matriz.MostrarMatriz();
             Console.WriteLine("---------------------------");
 
