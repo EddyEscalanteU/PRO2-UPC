ece5a8b230dd70adcc7f003444070063ed29e4e7
Eddy Escalante Ustariz <31227566+EddyEscalanteU@users.noreply.github.com>
2023-11-20 21:12:22 -0400

    serie 1

--- DOCENTE/matrix/Program.cs
+++ DOCENTE/matrix/Program.cs
@@ -33,7 +33,7 @@
             //Matriz.MostrarMatriz();
             */
             Console.WriteLine("---------------------------");
-            Matriz.LlenarMatrizTriangulo_v2();
+            Matriz.MatrizSerie_MasUno();
             Matriz.MostrarMatriz();
             Console.WriteLine("---------------------------");
 
