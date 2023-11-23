22dc456011ecbdca62a3800ca338f7e4e49a4aa6
= <=>
2023-11-22 20:59:42 -0400

    v1

--- angela/recursividad/Program.cs
+++ angela/recursividad/Program.cs
@@ -6,7 +6,7 @@
         {
            //long f = Factorial(64);
            //Console.WriteLine(f);
-           MostrarDigParesRecursivo(1151814);
+           GenerarSerieV1Recursivo(10);
         }   
         
         public static long Factorial(long n){
@@ -76,6 +76,28 @@
         }
 
 
-
+        public static void GenerarSerieV1Iterativo(int N){
+            int i = 1;
+            while(N > 0){
+                int serie = i + 5;
+                Console.WriteLine("Serie: " + serie); 
+                i = serie;  
+                N = N - 1;             
+            }
+        }
+         static int i = 1;
+        public static void GenerarSerieV1Recursivo(int N){
+           
+            if(N == 0){
+                return;
+            }else{
+               
+                int serie = i + 5;
+                Console.WriteLine("Serie: " + serie); 
+                i = serie; 
+                GenerarSerieV1Recursivo(N - 1);  
+            }
+              
+        }
     }
 }
