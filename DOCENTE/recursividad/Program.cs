d333749a0a95b1d6e1946d038b96b90ecc60831d
Eddy Escalante Ustariz <31227566+EddyEscalanteU@users.noreply.github.com>
2023-11-23 21:35:28 -0400

    Update Program.cs

--- DOCENTE/recursividad/Program.cs
+++ DOCENTE/recursividad/Program.cs
@@ -6,8 +6,22 @@
         {
            //long f = Factorial(64);
            //Console.WriteLine(f);
-           MostrarDigMultiplosMIterativo(78568168, 9);
+           MostrarDigMultiplosMRecursivo(78568168, 8);
         }  
+        public static void MostrarDigMultiplosMRecursivo(int N, int M)
+        {
+            int NroAux = N;
+            if(NroAux == 0){
+                return;
+            }else
+            {
+                int dig = NroAux % 10;
+                if(dig % M == 0){
+                    Console.WriteLine("Dig: " + dig);
+                }
+                MostrarDigMultiplosMRecursivo(NroAux / 10, M );
+            }
+        }
 
         public static void MostrarDigMultiplosMIterativo(int N, int M)
         {
