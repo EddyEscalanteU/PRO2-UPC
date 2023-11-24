3ffc17425abf7f52b5b71fc48a8b7dd201f9a67d
angeljoelsoto <149635631+angeljoelsoto@users.noreply.github.com>
2023-11-23 21:32:55 -0400

    avances clase dia jueves 23

--- jsoto/matrix/MatrizCadena.cs
+++ jsoto/matrix/MatrizCadena.cs
@@ -93,12 +93,4 @@ public class MatrizCadenas{
             y = y + 1;
             Insertar(x, y, peonAux);
     }
-
-    public void MoverCaballo(int x, int y){
-        string caballoAux = Obtener(x, y);
-        Insertar(x, y, defaultValue);
-        y = y + 2;
-        Insertar(x, y, caballoAux);
-    }
-    
 }
