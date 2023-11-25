ad961ec610509ef6307d8025289e93f6d2ebbf3b
JASSEPH <149634812+JASSEPH@users.noreply.github.com>
2023-11-24 20:31:40 -0400

    RECURSIVO

---
+++ jcastillo/SEGUNDO_PARCIAL/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Program
+{
+   static void Main()
+    {
+        // Puedes ingresar el número que desees para probar la función
+        int numero = 12345;
+        MostrarDigImparesRecursivos(numero);
+    }
+
+    public static void MostrarDigImparesRecursivos(int N)
+    {
+        if (N != 0)
+        {
+            int dig = N % 10;
+            if (dig % 2 != 0)
+            {
+                Console.WriteLine("Dig: " + dig);
+            }
+            MostrarDigImparesRecursivos(N / 10);
+        }
+    }
+}
