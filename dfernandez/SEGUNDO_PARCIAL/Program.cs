2a24bce2250c0d8cc5badaac1b51a4d5b2c744e2
juanonchon <132412187+juanonchon@users.noreply.github.com>
2023-11-24 20:01:52 -0400

    SEGUNDOPARCIAL

--- dfernandez/SEGUNDO_PARCIAL/Program.cs
+++ dfernandez/SEGUNDO_PARCIAL/Program.cs
@@ -1,6 +1,28 @@
-﻿namespace EXamen{
+﻿namespace SegundoParcial{
     class Program
     {
-        
+
+        //primer examen
+        public static void Main(string[] args)
+        {
+            MostrarDigImparesIterativo(7829367);
+
+        }
+        public static void MostrarDigImparesIterativo(int N)
+        {
+            int NroAux = N;
+            if(NroAux == 0){
+                return;
+            }else{
+                int dig = NroAux % 10;
+                if (dig  % 2 != 0){
+                    Console.WriteLine("Dig: " + dig);
+                }
+                MostrarDigImparesIterativo(NroAux / 10);
+               
+            }
+        }
+
+        //
     }
-}
+}    
