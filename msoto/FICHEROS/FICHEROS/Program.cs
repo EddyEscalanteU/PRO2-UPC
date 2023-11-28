0174f3ec6b862b3c5b3752cd1668574713502e2a
69misz <132626893+69misz@users.noreply.github.com>
2023-11-27 23:14:40 -0400

    Program.cs

--- msoto/FICHEROS/FICHEROS/Program.cs
+++ msoto/FICHEROS/FICHEROS/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+namespace ProgramacionDos{
+    public class Program{
+        public static void Main(string[] args)
+        {
+            Estudiante e1 =new Estudiante(5412367,54123670, "Jennifer", "Morales", "Saucedo", 18);
+            Estudiante e2 =new Estudiante(12654783,126547835, "Marilin", "Avila", "Carrasco", 21);
+            e1.Mostrar();
+            e2.Mostrar();
+        }
+    }
+}
