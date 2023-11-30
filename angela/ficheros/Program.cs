93a393c90d4036c95a1e35beb042452fcbcdb16e
= <=>
2023-11-29 22:00:35 -0400

    v2

--- angela/ficheros/Program.cs
+++ angela/ficheros/Program.cs
@@ -1,4 +1,4 @@
-﻿namespace ProgramacionDos{
+﻿﻿namespace ProgramacionDos{
     public class Program{
         public static void Main(string[] args)
         {
@@ -58,23 +58,83 @@
             e3.ArrayToEstudiante(estudianteFichero);
             e3.Mostrar();
             */
+
+           /* for(int i = 1000; i<=50000; i = i + 1000){
+                CrearArchivos(i+"", i);
+            }
+            */
+            /*
+            string path = "D:/emilio/fichero1.txt";
+            FicheroDocente fd = new FicheroDocente(path);
+            fd.EscribirTexto("hola mundo");
+            fd.path="D:/emilio/fichero2.txt";
+            string[] arrayEmilio = {"hola" , "mundo"};
+            fd.EscribirArray(arrayEmilio);
+            */
+            /*
+            int cant = 10;
+            ClaseArrayCadenas AC = new ClaseArrayCadenas(cant);
+            string[] resultadoSerie = AC.GenerarSerieExamenRecursivo(true, 1, 2, cant);
+            AC.MostrarArray();
+
+            string path = "D:/emilio/ficheroArray.txt";
+            FicheroDocente fd = new FicheroDocente(path);
+            fd.EscribirArray(resultadoSerie);
+            //Console.WriteLine();
+            */
+            /*
+            for(int a = 0; a<20;a++){
+            int cant = 10;
+            ClaseArrayCadenas AC = new ClaseArrayCadenas(cant);
+            long res =  AC.Factorial(cant);
+            //Console.WriteLine("res: " + res);
+            AC.MostrarArray();
+            
+            string path = "D:/emilio/FactorialArchivo" + a + ".txt";
+            FicheroDocente fd = new FicheroDocente(path);
+            fd.EscribirArray(AC.ArrayCadenas);
+                
+            }
+            */
+            //Console.WriteLine();
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+        public static void CrearArchivos(string subFolder, int cant){    
             DateTime timeInicio = DateTime.Now;
-            Console.WriteLine("Inicio: " + timeInicio);
+            //Console.WriteLine("Inicio: " + timeInicio);
             Fichero f =  new Fichero();
-            for(int i = 1; i<=1000;i++){
+            for(int i = 1; i<=cant;i++){               
+                string pathFor = "D:/GitHub/PRO2-UPC/DOCENTE/ficheros/for/" + subFolder;
+                bool exists = Directory.Exists(pathFor);
+
+                if(!exists){
+                    Directory.CreateDirectory(pathFor);
+                }    
+
                 Estudiante e = new Estudiante(9775810, 2023, 
                 "Douglas ", "Carrasco ", " Cruz", 19);
                 
                 string[] arrayEstudiante = e.EstudianteToArray();
 
-                string pathFor = "D:/UPC/PRO2-UPC/angela/ficheros/for/" + i + ".txt";
+                pathFor = pathFor + "/" + i + ".txt";
+                Console.WriteLine(pathFor);
                 f.WriteLine(pathFor, arrayEstudiante);
             }
             DateTime timeFin = DateTime.Now;
-            Console.WriteLine("Fin: " + timeFin);
+            //Console.WriteLine("Fin: " + timeFin);
             
-            Console.WriteLine("Total: " + timeFin.Subtract(timeInicio));
-
+            Console.WriteLine("Total: " + "(" + cant + ") " + timeFin.Subtract(timeInicio));
         }
     }
 }
